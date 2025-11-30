89b695e46e82b682ac74ce63c04d430db9a463f3
yumimeko <131466542+yumimekoo@users.noreply.github.com>
2025-11-30 14:34:11 +0100

    added gamestate static

---
+++ Assets/Scripts/GameStateManager/GameState.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public static class GameState
+{
+    // --- Player Movement Control ---
+    public static bool playerMovementAllowed = true;
+    public static bool playerInteractionAllowed = true;
+}
