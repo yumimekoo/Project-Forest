f39fa02e40d2187b3a9c03212da80e60e2d059f8
yumimeko <131466542+yumimekoo@users.noreply.github.com>
2025-11-30 21:48:36 +0100

    added a storage drawer ( Closes #13 ) same functionality as the fridge (also copy paste code and even uses the fridgeUI (maybe we should rename it to storageDisplayUI))

--- Assets/Scripts/UnlockSystem/UnlockDatabaseSO.cs
+++ Assets/Scripts/UnlockSystem/UnlockDatabaseSO.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "UnlockDatabaseSO", menuName = "Unlock/UnlockDatabase")]
 public class UnlockDatabaseSO : ScriptableObject
 {
+    [Header("Fridge Items")]
     public List<ItemDataSO> fridgeItems; // all frigde items
     public List<ItemDataSO> unlockedFridgeItems = new List<ItemDataSO>(); // unlocked fridge items
 
-    public bool IsUnlocked(ItemDataSO item) => unlockedFridgeItems.Contains(item);
+    [Header("Storage Items")]
+    public List<ItemDataSO> storageItems; // all storage items
+    public List<ItemDataSO> unlockedStorageItems = new List<ItemDataSO>(); // unlocked storage items
+   /// public bool IsUnlocked(ItemDataSO item) => unlockedFridgeItems.Contains(item);
 
     public List<ItemDataSO> GetUnlockedFridgeItems() => unlockedFridgeItems;
+    public List<ItemDataSO> GetUnlockedStorageItems() => unlockedStorageItems;
 }
