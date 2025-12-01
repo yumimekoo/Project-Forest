a3e99c1066418c2f0cf3a6d2eb9b8e7c4664e933
yumimeko <131466542+yumimekoo@users.noreply.github.com>
2025-12-01 22:31:56 +0100

    added TimeManager singleton ( Closes #15 ) sends events at switch and end of the day, also changes GameState bools isDay and isNight

---
+++ Assets/Scripts/UI/TimeUI/TimeUI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TimeUI : MonoBehaviour
+{
+    public UIDocument timeUI;
+    private ProgressBar progressBar;
+    private Label timeLabel;
+    private VisualElement fill;
+
+    private void OnEnable()
+    {
+        var root = timeUI.rootVisualElement;
+        timeLabel = root.Q<Label>("timeLabel");
+        progressBar = root.Q<ProgressBar>("progressTime");
+        fill = progressBar.Q<VisualElement>(className: "unity-progress-bar__progress");
+        if (TimeManager.Instance != null)
+        {
+            TimeManager.Instance.OnTimeChanged += UpdateTime;
+            TimeManager.Instance.OnProgressChanged += UpdateProgress;
+            TimeManager.Instance.OnNightTriggered += HandleNightTriggered;
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        if (TimeManager.Instance != null)
+        {
+            TimeManager.Instance.OnTimeChanged -= UpdateTime;
+            TimeManager.Instance.OnProgressChanged -= UpdateProgress;
+            TimeManager.Instance.OnNightTriggered -= HandleNightTriggered;
+        }
+    }
+
+    private void UpdateTime(float currentTime)
+    {
+        int seconds = Mathf.FloorToInt(currentTime);
+        int mins = seconds / 60;
+        int secs = seconds % 60;
+
+        timeLabel.text = $"{mins:00}:{secs:00}";
+    }
+
+    private void UpdateProgress(float progress)
+    {
+        Debug.Log($"Updating progress to {progress}");
+        progressBar.value = progress;
+    }
+
+    private void HandleNightTriggered()
+    {
+        fill.style.backgroundColor = new Color(0.08f, 0.01f, 0.28f);
+    }
+}
