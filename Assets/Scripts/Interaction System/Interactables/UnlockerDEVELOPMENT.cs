e5dc54be8f0d9b1b44450292872360673b0b08a4
yumimeko <131466542+yumimekoo@users.noreply.github.com>
2025-12-15 17:55:43 +0100

    unlocker adjusted for further development ( Closes #47 )

--- Assets/Scripts/Interaction System/Interactables/UnlockerDEVELOPMENT.cs
+++ Assets/Scripts/Interaction System/Interactables/UnlockerDEVELOPMENT.cs
@@ -1,18 +1,25 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UnlockerDEVELOPMENT : MonoBehaviour, IInteractable
 {
-    [SerializeField] ItemDataSO itemToUnlock;
-    [SerializeField] ItemDataSO itemToUnlock2;
+    private int counter = 1;
+    private List<ItemDataSO> items;
+
+    public void Start()
+    {
+        items = new List<ItemDataSO>(Resources.LoadAll<ItemDataSO>("ScriptableObjectsData/ItemData/Base"));
+    }
     public string GetInteractionPrompt()
     {
-        return $"Unlock {itemToUnlock.itemName}";
+        return $"Unlock item with ID: {counter}";
     }
 
     public void Interact(PlayerInventory player)
     {
-        UnlockManager.Instance.UnlockItem(itemToUnlock);
-        UnlockManager.Instance.UnlockItem(itemToUnlock2);
+        UnlockManager.Instance.UnlockItem(items.Find(item => item.id == counter));
+        counter++;
     }
 
 }
