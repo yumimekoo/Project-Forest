09b473c46ebe3408f43b904c83157c9ac59b9d5b
yumimeko <131466542+yumimekoo@users.noreply.github.com>
2025-12-20 19:19:43 +0100

    npc walks to free chairs and sits down  + navmesh pathfinding (Closes #34)

--- Assets/Scripts/NPCSystem/NPCSpawner.cs
+++ Assets/Scripts/NPCSystem/NPCSpawner.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 public class NPCSpawner : MonoBehaviour
 {
     public NPCIdentitySO npcToSpawn;
+
+    private float spawnInterval = 5f;
+    private float timeSinceLastSpawn = 0f;
+
+    private void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= spawnInterval && ChairManager.Instance.GetFreeChair() != null)
+        {
+            Spawn();
+            timeSinceLastSpawn = 0f;
+        }
+    }
     // pooling later on
     public void Spawn()
     {
